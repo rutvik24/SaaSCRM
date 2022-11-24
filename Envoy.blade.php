a17be218be6cc39a9d6d2116098fb9918ff740f3
rutvik2001 <rutvik@atliq.com>
2022-11-24 17:37:24 +0530

    Changed: Envoy for another site

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,7 +1,7 @@
 @servers(['web' => 'ubuntu@129.154.232.195'])
 
 @task('addDB', ['on' => 'web'])
-    sudo /bin/bash /usr/bin/clpctl db:add --domainName=demo.rutviknabhoya.me --databaseName={{ $db_name }} --databaseUserName={{ $db_username }} --databaseUserPassword={{ $db_password }}
+    sudo /bin/bash /usr/bin/clpctl db:add --domainName=app.rutviknabhoya.me --databaseName={{ $db_name }} --databaseUserName={{ $db_username }} --databaseUserPassword={{ $db_password }}
 @endtask
 
 @task('delete-db', ['on' => 'web'])
